a5ded16ab1f92cb6d2b084ee32777a24258555de
weibiao <ybilland@163.com>
2017-04-06 19:29:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bill/scalingtextview/ExpandableTextView.java
@@ -0,0 +1,107 @@
+package com.example.bill.scalingtextview;
+
+import android.content.Context;
+import android.support.annotation.ColorRes;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Bill on 2017/4/6.
+ */
+
+public class ExpandableTextView extends LinearLayout implements View.OnClickListener {
+
+    private Context context;
+    private TextView contentText;
+    private TextView scalingText;
+
+    private static final int CONTENT_DESC_MAX_LINE = 2;// 默认展示最大行数3行
+    private static final int SHRINK_UP_STATE = 1;// 收起状态
+    private static final int SPREAD_STATE = 2;// 展开状态
+    private int mState;
+
+    public ExpandableTextView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public ExpandableTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public ExpandableTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        this.context = context;
+        View view = View.inflate(context, R.layout.layout_expandable_textview, null);
+        addView(view);
+        contentText = (TextView) view.findViewById(R.id.tv_content);
+        scalingText = (TextView) view.findViewById(R.id.tv_scaling);
+        scalingText.setOnClickListener(this);
+    }
+
+    public void setText(String text) {
+        contentText.setMaxLines(Integer.MAX_VALUE);
+        contentText.setText(text);
+        contentText.post(new Runnable() {
+            @Override
+            public void run() {
+                int contentLine = contentText.getLineCount();
+                if (contentLine > CONTENT_DESC_MAX_LINE) {
+                    scalingText.setVisibility(VISIBLE);
+                    putAway();
+                } else {
+                    scalingText.setVisibility(GONE);
+                    unFold();
+                }
+            }
+        });
+    }
+
+    public void setTextSize(float size) {
+        contentText.setTextSize(size);
+    }
+
+    public void setTextColor(@ColorRes int id) {
+        contentText.setTextColor(context.getResources().getColor(id));
+    }
+
+    /**
+     * 收起
+     */
+    private void putAway() {
+        contentText.setMaxLines(CONTENT_DESC_MAX_LINE);
+        contentText.requestLayout();
+        scalingText.setText("显示全部");
+        mState = SHRINK_UP_STATE;
+    }
+
+    /**
+     * 展开
+     */
+    private void unFold() {
+        contentText.setMaxLines(Integer.MAX_VALUE);
+        contentText.requestLayout();
+        scalingText.setText("收起");
+        mState = SPREAD_STATE;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == scalingText) {
+            if (mState == SPREAD_STATE) {
+                putAway();
+            } else if (mState == SHRINK_UP_STATE) {
+                unFold();
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/bill/scalingtextview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.bill.scalingtextview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ExpandableTextView expandableTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        expandableTextView = (ExpandableTextView) findViewById(R.id.expand);
+
+        expandableTextView.setText("发色纺纱的方式的方式的方式的方式的发色纺纱的方式的方式的方式的方式的发色纺纱的方式的发色纺纱的方式的的方式的");
+    }
+}
